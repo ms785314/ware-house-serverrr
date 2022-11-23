1527ec6b127569d84c54c309a408c30f44d07323
ms785314 <ms785314@gmail.com>
2022-11-23 20:27:01 +0600

    modified

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config()
 
-app.use(cors());
+// app.use(cors());
+
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
